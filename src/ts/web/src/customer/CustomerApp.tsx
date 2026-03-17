d1e7aeb0e05db10425d1c8f2165a744ac12e5473
demo-crafting <120827971+demo-crafting@users.noreply.github.com>
2026-03-17 13:29:22 -0700

    Wrap CustomerApp with ThemeProvider

--- src/ts/web/src/customer/CustomerApp.tsx
+++ src/ts/web/src/customer/CustomerApp.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { Product } from '../shared/types';
+import { ThemeProvider } from '../shared/ThemeContext';
 import LandingPage from './pages/LandingPage';
 import ProductPage from './pages/ProductPage';
 import CheckoutPage from './pages/CheckoutPage';
@@ -57,40 +58,42 @@ const CustomerApp: React.FC = () => {
   };
 
   return (
-    <div>
-      {currentPage === 'landing' && (
-        <LandingPage
-          cartItemCount={cartItemCount}
-          onProductClick={handleProductClick}
-          onCartClick={handleGoToCheckout}
-        />
-      )}
-      {currentPage === 'product' && selectedProduct && (
-        <ProductPage
-          product={selectedProduct}
-          onAddToCart={handleAddToCart}
-          onClose={handleCloseProductPage}
-        />
-      )}
-      {currentPage === 'checkout' && cart && (
-        <CheckoutPage
-          cart={cart}
-          onUpdateQuantity={handleUpdateQuantity}
-          onCheckout={handleGoToPayment}
-          onBack={handleBackToLanding}
-        />
-      )}
-      {currentPage === 'payment' && cart && (
-        <PaymentPage
-          totalPrice={cart.totalPrice}
-          onPlaceOrder={handlePlaceOrder}
-          onBack={() => setCurrentPage('checkout')}
-        />
-      )}
-      {currentPage === 'thankyou' && (
-        <ThankYouPage onContinueShopping={handleBackToLanding} />
-      )}
-    </div>
+    <ThemeProvider>
+      <div>
+        {currentPage === 'landing' && (
+          <LandingPage
+            cartItemCount={cartItemCount}
+            onProductClick={handleProductClick}
+            onCartClick={handleGoToCheckout}
+          />
+        )}
+        {currentPage === 'product' && selectedProduct && (
+          <ProductPage
+            product={selectedProduct}
+            onAddToCart={handleAddToCart}
+            onClose={handleCloseProductPage}
+          />
+        )}
+        {currentPage === 'checkout' && cart && (
+          <CheckoutPage
+            cart={cart}
+            onUpdateQuantity={handleUpdateQuantity}
+            onCheckout={handleGoToPayment}
+            onBack={handleBackToLanding}
+          />
+        )}
+        {currentPage === 'payment' && cart && (
+          <PaymentPage
+            totalPrice={cart.totalPrice}
+            onPlaceOrder={handlePlaceOrder}
+            onBack={() => setCurrentPage('checkout')}
+          />
+        )}
+        {currentPage === 'thankyou' && (
+          <ThankYouPage onContinueShopping={handleBackToLanding} />
+        )}
+      </div>
+    </ThemeProvider>
   );
 };
 
